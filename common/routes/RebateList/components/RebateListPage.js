c720e0036a12ec0463b1f4ac61cd31018a50d54f
chrisfitkin <cfitkin@gmail.com>
2016-12-16 05:10:33 -0800

    Test stonecutter library

--- common/routes/RebateList/components/RebateListPage.js
+++ common/routes/RebateList/components/RebateListPage.js
@@ -4,6 +4,9 @@ import RebateListItem from '../components/RebateListItem'
 import Helmet from 'react-helmet'
 import { StyleSheet, css } from 'aphrodite'
 
+import { CSSGrid, SpringGrid, measureItems, makeResponsive, layout } from 'react-stonecutter';
+
+
 const styles = StyleSheet.create({
   root: {
     width: 'auto',
@@ -28,6 +31,14 @@ const sizes = [
   { mq: '1024px', columns: 3, gutter: 20 }
 ]
 
+const CustomGrid = makeResponsive(measureItems(CSSGrid), {
+  maxWidth: 1920,
+  minPadding: 100
+});
+
+const Grid = measureItems(SpringGrid);
+
+
 const RebateListPage = ({ rebates, hasMore, loadMore }) => (
   <div className={css(styles.root)}>
     <Helmet title='Rebates' />
@@ -36,18 +47,22 @@ const RebateListPage = ({ rebates, hasMore, loadMore }) => (
         <h2 className={css(styles.title)}>Loading....</h2>
       </div>}
     {!rebates.isLoading &&
-      <MasonryInfiniteScroller
-        sizes={sizes}
-        loader={null}
-        style={styles.masonryInfiniteScroller}
-        className={css(styles.masonryInfiniteScroller)}
-        hasMore={hasMore}
-        loadMore={loadMore}
+      <Grid
+          component="div"
+          columns={3}
+          columnWidth={300}
+          gutterWidth={5}
+          gutterHeight={5}
+          layout={layout.pinterest}
+          duration={800}
+          easing="ease-out"
         >
         {
-          rebates.data.map((rebate, i) => <RebateListItem key={rebate.uuid} rebate={rebate} />)
+          rebates.data.map((rebate, i) => 
+            <RebateListItem key={rebate.uuid} rebate={rebate} />
+          )
         }
-      </MasonryInfiniteScroller>}
+      </Grid>}
     {rebates.isLoadingMore &&
       <div>
         <h2 className={css(styles.title)}>Loading More...</h2>
